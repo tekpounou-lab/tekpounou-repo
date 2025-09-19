6646d74599f5503a589ab2f0ce18d3efdfbd783d
SaasHaiti1 <christvivantmultiservices@gmail.com>
2025-09-19 03:14:51 -0400

    Fix SupabaseProvider import to use path alias

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 // Providers
-import SupabaseProvider from './components/providers/SupabaseProvider.tsx'
+import SupabaseProvider from '@/components/providers/SupabaseProvider'
 import AuthProvider from './components/providers/AuthProvider'
 
 // Components
